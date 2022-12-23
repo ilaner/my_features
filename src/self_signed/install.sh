05b3cc74b36fb8b023f8752ab99ed1709e9d57aa
ilaner <88267954+ilaner@users.noreply.github.com>
2022-12-23 14:06:22 +0200

    try now

--- src/self_signed/install.sh
+++ src/self_signed/install.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 set -e
 
-echo "Activating feature 'color'"
-echo "The provided favorite color is: ${FAVORITE}"
+# This script should run in the devcontainer.
+# This script creates a local certificate file.
+# It tries to connect to a server (for instance, github), show the local certificates and save it to a file.
 
-cat > /usr/local/bin/color \
-<< EOF
-#!/bin/sh
-echo "my favorite color is ${FAVORITE}"
-EOF
+# We connect to a random server and not paloaltonetworks.com to get external certificates. 
+# if command fails 
 
-chmod +x /usr/local/bin/color
+if curl -I https://github.com > /dev/null; then
+    echo "No need to update certificate"
+    exit
+fi
+
+CONNECT_SERVER="github.com:443"
+
+FILE=/usr/local/share/ca-certificates/certs.crt
+
+REGEX_BEGIN="/^-----BEGIN CERTIFICATE-----$/"
+REGEX_END="/^-----END CERTIFICATE-----$"
+
+# Parse the certificate to a file
+openssl s_client -showcerts -connect $CONNECT_SERVER | \
+    sed -n "$REGEX_BEGIN,$REGEX_END/p" > "$FILE"
+
+if [ ! -f "$FILE" ]; then
+    echo "Failed getting the certificates, no output file was created."
+    exit
+fi
+
+git config --system http.sslCAInfo $FILE
+
+
